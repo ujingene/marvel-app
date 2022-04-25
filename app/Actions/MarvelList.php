3239c467852ecd27cf40a5847b9fb229974bea0f
Eugene Wanjira <ujingene@users.noreply.github.com>
2022-04-25 19:20:09 +0300

    Cache marvel list response
    - set cache to use database
    - create cache table
    - cache response from api

--- app/Actions/MarvelList.php
+++ app/Actions/MarvelList.php
@@ -3,6 +3,7 @@
 namespace App\Actions;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 /**
@@ -10,19 +11,23 @@ use Illuminate\Support\Facades\Http;
  */
 class MarvelList
 {
+	public function __construct()
+	{
+		$this->apiKey = config('services.marvel.key');
+		$this->secret = config('services.marvel.secret');
+		$this->ts 	= Carbon::now()->toDateTimeString();
+		$this->hash 	= md5($this->ts . $this->secret . $this->apiKey);
+	}
 	public function execute($endpoint)
 	{
-		$apiKey = config('services.marvel.key');
-		$secret = config('services.marvel.secret');
-		$ts 	= Carbon::now()->toDateTimeString();
-		$hash 	= md5($ts . $secret . $apiKey);
-
-		$response = Http::get(config('services.marvel.api_url') . $endpoint, [
-			'ts' 		=> $ts,
-            'apikey' 	=> $apiKey,
-            'hash' 		=> $hash,
+		$response = Cache::remember('marvel_characters', 300, function() use($endpoint) {
+		    return Http::get(config('services.marvel.api_url') . $endpoint, [
+			'ts' 		=> $this->ts,
+            'apikey' 	=> $this->apiKey,
+            'hash' 		=> $this->hash,
             'limit'		=> 5
         ]);
+		});
 
         $characters = json_decode($response->body());
 
