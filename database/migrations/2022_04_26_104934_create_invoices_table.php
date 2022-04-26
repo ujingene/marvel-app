c104d6b0222cbbca27e5a7942aa7e7b30d45d4bf
Eugene Wanjira <ujingene@users.noreply.github.com>
2022-04-26 13:58:52 +0300

    wip
    - created new invoice model and migration file

---
+++ database/migrations/2022_04_26_104934_create_invoices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('InvoiceNo');
+            $table->string('StockCode');
+            $table->string('Description');
+            $table->integer('Quantity');
+            $table->date('InvoiceDate');
+            $table->float('UnitPrice');
+            $table->bigInteger('CustomerID');
+            $table->string('Country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
