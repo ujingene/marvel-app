5846d74989b28e1324c6b18731be75209dd454ae
Eugene Wanjira <ujingene@users.noreply.github.com>
2022-04-27 02:48:52 +0300

    added jobs and flags migration file

---
+++ database/migrations/2022_04_26_205956_create_flags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flags', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name')->unique();
+            $table->boolean('imported')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flags');
+    }
+}
