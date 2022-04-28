81603603b1cf9a33e3f7e779f2be3b1bc8c11216
Eugene Wanjira <ujingene@users.noreply.github.com>
2022-04-28 12:11:03 +0300

    test api integration

--- tests/Unit/MarvelApiTest.php
+++ tests/Unit/MarvelApiTest.php
@@ -1,52 +1,39 @@
 <?php
 
-use App\Actions\MarvelList;
 use Carbon\Carbon;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Http;
-
-uses(CreatesApplication::class);
-uses(Tests\TestCase::class, RefreshDatabase::class);
-
-beforeEach()->createApplication();
+use Illuminate\Http\Client\ConnectionException;
 
 it('can test remote marvel address', function () {
-    $api_url = config('services.marvel.api_url', 'https://gateway.marvel.com/v1/public');
-    $response = Http::get($api_url);
+    $response = Http::get($this->api_url);
 
-    $this->assertEquals(404, $response->status());
+    $this->assertEquals(200, $response->status());
 });
 
 it('can test api authentication', function () {
-    $api_url = config('services.marvel.api_url', 'https://gateway.marvel.com/v1/public');
-    $secret = config('services.marvel.secret');
-    $apiKey = config('services.marvel.key');
-
-    $ts = Carbon::now()->toDateTimeString();
-
-    $hash = md5($ts . $secret . $apiKey);
-
-    $queryString = [
-        'ts' => $ts,
-        'apikey' => $apiKey,
-        'hash' => $hash,
-        'limit' => 1
-    ];
-
-    $url = $api_url . 'characters?' . http_build_query($queryString);
-
-    $response = Http::get($url);
+    $response = Http::retry(3, 500, function ($exception) {
+        return $exception instanceof ConnectionException;
+    })->get($this->api_url, 
+    [
+        'ts'        => Carbon::now()->toDateTimeString(),
+        'apikey'    => 'jkdf489yusdfci93jksbdfs093',
+        'hash'      => '93485zjT487583039jdnf948nj'
+    ]);
 
     $this->assertEquals(200, $response->status());
 });
 
 it('can test successfull authentication can pull marvel characters', function () {
-    $getMarvelCharacters = new MarvelList;
-
-    $response = $getMarvelCharacters->execute($endpoint='characters');
+    $response = Http::retry(3, 500, function ($exception) {
+        return $exception instanceof ConnectionException;
+    })->get($this->api_url, 
+    [
+        'ts'        => Carbon::now()->toDateTimeString(),
+        'apikey'    => 'jkdf489yusdfci93jksbdfs093',
+        'hash'      => '93485zjT487583039jdnf948nj'
+    ]);
 
     $this->assertEquals(200, $response->status());
     
-    expect($response)->toBeArray();
+    expect($response['results'])->toBeArray();
 });
 
